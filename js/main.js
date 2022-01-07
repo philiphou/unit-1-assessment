b01723eba889963a5a2b403bfea524d20dee976d
Pengcheng (Philip) Hou <hphilip168@gmail.com>
2022-01-07 17:48:18 -0500

    modify

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ num.value=1;
 function addNum(){
     addSum=Number(result.textContent)+Number(num.value);
     result.textContent=addSum;
+    addSum<0? result.style.color="red":result.style.color="black";
 }
 function minusNum(){
     minusSum=Number(result.textContent)-Number(num.value);
