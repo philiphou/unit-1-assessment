9de2842ba431b1030a6d6ab4c074175653d7df9a
Pengcheng (Philip) Hou <hphilip168@gmail.com>
2022-01-07 17:22:51 -0500

    Initial  commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// set variables//
+var result=document.getElementById("result");
+var num=document.getElementById("factor");
+var plus=document.getElementById("plus");
+var minus=document.getElementById("minus");
+num.value=1;
+
+function addNum(){
+    addSum=Number(result.textContent)+Number(num.value);
+    result.textContent=addSum;
+}
+function minusNum(){
+    minusSum=Number(result.textContent)-Number(num.value);
+    result.textContent=minusSum
+    minusSum<0? result.style.color="red":result.style.color="black"
+}
+
+//event listeners//
+plus.addEventListener("click",addNum);
+minus.addEventListener("click",minusNum);
+
+
+
+
+
